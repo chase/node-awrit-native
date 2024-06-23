f3f504ef603b16f4c4097d8bd478feb18ab260c1
Chase Colman <chase@colman.io>
2024-06-24 01:02:06 +0800

    Fix install script

---
+++ install.mjs
@@ -0,0 +1,26 @@
+import fetch from "node-fetch";
+import { execSync } from "node:child_process";
+import { existsSync, readFileSync } from "node:fs";
+import { arch, platform } from "node:os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { x } from "tar";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pkg = JSON.parse(readFileSync(join(__dirname, "package.json"), "utf8"));
+
+const GITHUB_REPO = "chase/node-shm-write";
+const url = `https://github.com/${GITHUB_REPO}/releases/download/v${pkg.version}/${platform()}-${arch()}.tar.gz`;
+
+if (!existsSync(join(__dirname, "prebuilds", `${platform()}-${arch()}`))) {
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) throw new Error(`Failed to fetch ${url}`);
+      return res.body.pipe(x({ C: __dirname }));
+    })
+    .catch((err) => {
+      console.error(err);
+      execSync("npm run prebuild");
+    });
+}
