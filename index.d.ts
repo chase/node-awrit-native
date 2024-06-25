ed716cfa8036004f29599a5f01f7577b1fe9b9e6
Chase <chase@macro.com>
2024-06-26 05:49:42 +0800

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,7 @@
 declare export function shmWrite(name: string, buffer: Buffer): void;
 /** releases the shared memory at name */
 declare export function shmUnlink(name: string): void;
+/** sets termios attributes to allow realtime updates for key input */
+declare export function setupTermios(): void;
+/** restores termios attributes to the original attributes before calling setupTermios */
+declare export function cleanupTermios(): void;
