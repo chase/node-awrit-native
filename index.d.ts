0c12bfafae1746494380e1a271a75d63bf4b634d
Chase <chase@colman.io>
2025-02-18 11:08:26 -0500

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="node">
 
-/** allocates and writes buffer to the shared memory at name */
-export declare function shmWrite(name: string, buffer: Buffer): void;
+/** allocates and writes buffer to the shared memory at name, optionally applying a BGRA to RGBA fix */
+export declare function shmWrite(
+	name: string,
+	buffer: Buffer,
+	rgbaFix?: boolean,
+): void;
 /** releases the shared memory at name */
 export declare function shmUnlink(name: string): void;
 /** sets termios attributes to allow realtime updates for key input */
@@ -10,78 +14,78 @@ export declare function setupInput(): void;
 export declare function cleanupInput(): void;
 
 export declare enum KeyEvent {
-  Invalid = 0,
-  Down = 1,
-  Repeat = 2,
-  Up = 3,
-  Unicode = 4,
+	Invalid = 0,
+	Down = 1,
+	Repeat = 2,
+	Up = 3,
+	Unicode = 4,
 }
 
 export declare enum MouseEvent {
-  Down = 0,
-  Up = 1,
-  Move = 2,
+	Down = 0,
+	Up = 1,
+	Move = 2,
 }
 
 export declare enum EscapeType {
-  None = 0,
-  CSI = 1,
-  OSC = 2,
-  DCS = 3,
-  PM = 4,
-  SOS = 5,
-  APC = 6,
-  Key = 7,
-  Mouse = 8,
+	None = 0,
+	CSI = 1,
+	OSC = 2,
+	DCS = 3,
+	PM = 4,
+	SOS = 5,
+	APC = 6,
+	Key = 7,
+	Mouse = 8,
 }
 
 export declare enum MouseModifier {
-  Shift = 1 << 2,
-  Alt = 1 << 3,
-  Ctrl = 1 << 4,
-  Motion = 1 << 5,
-  None = 0,
+	Shift = 1 << 2,
+	Alt = 1 << 3,
+	Ctrl = 1 << 4,
+	Motion = 1 << 5,
+	None = 0,
 }
 
 export declare enum MouseButton {
-  Left = 1 << 0,
-  Middle = 1 << 1,
-  Right = 1 << 2,
-  Fourth = 1 << 3,
-  Fifth = 1 << 4,
-  Sixth = 1 << 5,
-  Seventh = 1 << 6,
-  WheelUp = 1 << 7,
-  WheelDown = 1 << 8,
-  WheelLeft = 1 << 9,
-  WheelRight = 1 << 10,
-  None = 0,
+	Left = 1 << 0,
+	Middle = 1 << 1,
+	Right = 1 << 2,
+	Fourth = 1 << 3,
+	Fifth = 1 << 4,
+	Sixth = 1 << 5,
+	Seventh = 1 << 6,
+	WheelUp = 1 << 7,
+	WheelDown = 1 << 8,
+	WheelLeft = 1 << 9,
+	WheelRight = 1 << 10,
+	None = 0,
 }
 
 export type InputEvent =
-  | {
-      type: EscapeType.Key;
-      event: KeyEvent;
-      code: string;
-    }
-  | {
-      type: EscapeType.Mouse;
-      event: MouseEvent;
-      buttons: number;
-      modfiers: number;
-      x?: number;
-      y?: number;
-    }
-  | {
-      type:
-        | EscapeType.CSI
-        | EscapeType.OSC
-        | EscapeType.DCS
-        | EscapeType.PM
-        | EscapeType.SOS
-        | EscapeType.APC;
-      data: string;
-    };
+	| {
+			type: EscapeType.Key;
+			event: KeyEvent;
+			code: string;
+	  }
+	| {
+			type: EscapeType.Mouse;
+			event: MouseEvent;
+			buttons: number;
+			modfiers: number;
+			x?: number;
+			y?: number;
+	  }
+	| {
+			type:
+				| EscapeType.CSI
+				| EscapeType.OSC
+				| EscapeType.DCS
+				| EscapeType.PM
+				| EscapeType.SOS
+				| EscapeType.APC;
+			data: string;
+	  };
 
 /**
  * Listens for input events at intervalMs, defaults to 10ms
@@ -90,6 +94,6 @@ export type InputEvent =
  * @returns Cancel callback
  */
 export declare function listenForInput(
-  cb: (evt: InputEvent) => void,
-  intervalMs?: number,
+	cb: (evt: InputEvent) => void,
+	intervalMs?: number,
 ): () => void;
